b57e04193c406057f7152b2f0ce07d21a2148b16
Ayomide <ayomidedans@gmail.com>
2024-11-10 23:07:35 +0100

    feat: footer design

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -2,8 +2,6 @@
   /* eslint-disable @next/next/no-img-element */
 }
 
-import React from 'react';
-import Link from 'next/link';
 
 import { Header } from '../../../payload/payload-types';
 import { fetchHeader } from '../../_api/fetchGlobals';
